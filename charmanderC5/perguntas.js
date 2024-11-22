52d3c3803727e4c76cb742d988165c3bd0809a59
charmander charmeleon charizard maga X Y <angelogabeiel6381@gmail.com>
2024-11-22 09:14:44 -0300

    Add files via upload

---
+++ charmanderC5/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'POKEMON',
+    'QUAL POKEMON COM MAIS ATAQUE DE TIPO AGUA?',
+    'O DEUS DO ESPAÇO O LENDARIO PALKIA'
+)
+
+criaCartao(
+    'POKEMON',
+    'QUAL O NOME DO DEUS POKEMON?',
+    'ARCEUS'
+)
+
+criaCartao(
+    'POKEMON',
+    'Qual o melhor pokemon de Kalos?',
+    'Greninja'
+)
+
+criaCartao(
+    'Fire Red',
+    'Qual o melhor pokemon inicial em fire red',
+    'Charmander'
+)
